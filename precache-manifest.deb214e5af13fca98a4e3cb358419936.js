34caa92ce3b7a5aebcef750d603a171d29418086
Innocent1312 <anthonydev33@gmail.com>
2019-12-10 23:12:22 +0200

    Updates

---
+++ precache-manifest.deb214e5af13fca98a4e3cb358419936.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75060d6602ebfdf74dd10eeeae35bb9d",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "6ba8f83bccdcabd0efa5",
+    "url": "/react_add-todo-form/static/css/main.2fbfc203.chunk.css"
+  },
+  {
+    "revision": "8c62e9e39566e04c3c1e",
+    "url": "/react_add-todo-form/static/js/2.263e448f.chunk.js"
+  },
+  {
+    "revision": "6ba8f83bccdcabd0efa5",
+    "url": "/react_add-todo-form/static/js/main.a6a0f039.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
